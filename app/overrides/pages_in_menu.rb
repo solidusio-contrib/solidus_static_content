dafa7d37f8e9fda0afd8d4854f493697a8698e65
Davor Ocelic <docelic@spinlocksolutions.com>
2016-12-28 23:14:00 +0100

    Remove Deface hook for Pages in favor of MenuItem
    This commit replaces the Deface hook with a standard MenuItem approach to
    add the "Pages" entry into the Admin menu.
    
    Besides removing Deface, this change has an additional benefit that the
    Pages entry now behaves in the same way that the original Solidus admin
    menu entries do.
    (Previously, with Deface, the "Pages" entry was being added to the menu
    even on the Admin Login page, where the standard menu should remain empty.)

--- app/overrides/pages_in_menu.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
-                     :name => "static_content_pages_admin_tab",
-                     :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:pages, label: 'Pages', icon: 'file-text') %>",
-                     :disabled => false)
--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -7,9 +7,11 @@ module SpreeStaticContent
     config.autoload_paths += %W(#{config.root}/lib)
 
     def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-      end
+       Spree::Backend::Config.menu_items << Spree::Backend::Config.class::MenuItem.new(
+        [:pages],
+        'file-text',
+        condition: -> { can?(:admin, Spree::Page) },
+      )
     end
 
     config.to_prepare &method(:activate).to_proc
