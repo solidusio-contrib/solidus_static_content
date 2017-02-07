867d4e81ee962b83d576515be6f57a14b1809013
John Hawthorn <john@stembolt.com>
2017-02-07 13:58:26 -0800

    Use normal get/post/put/delete test helpers

--- spec/controllers/static_content_controller_spec.rb
+++ spec/controllers/static_content_controller_spec.rb
@@ -11,26 +11,26 @@ describe Spree::StaticContentController, type: :controller do
     it 'accepts path as root' do
       page = create(:page, slug: '/', stores: [store])
       request.path = page.slug
-      spree_get :show, path: page.slug
+      get :show, path: page.slug
       expect(response).to be_success
     end
 
     it 'accepts path as string' do
       page = create(:page, slug: 'hello', stores: [store])
       request.path = page.slug
-      spree_get :show, path: page.slug
+      get :show, path: page.slug
       expect(response).to be_success
     end
 
     it 'accepts path as nested' do
       page = create(:page, slug: 'aa/bb/cc', stores: [store])
       request.path = page.slug
-      spree_get :show, path: page.slug
+      get :show, path: page.slug
       expect(response).to be_success
     end
 
     it 'respond with a 404 when no page exists' do
-      spree_get :show
+      get :show
       expect(response.response_code).to eq(404)
     end
   end
