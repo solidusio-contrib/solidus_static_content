a13c3dd6f7054e15b3d5cf976c404433a9aa1c16
Thomas von Deyen <thomas@vondeyen.com>
2017-08-15 14:11:16 +0200

    Fix the admin tab
    In Solidus < 1.4 we still need a Deface override, but need to check the permissions of the user, so it does not appear in the login screen.
    
    For Solidus >= 1.4 we use the `Spree::Config::Backend::MenuItem` configuration.

--- lib/spree_static_content/engine.rb
+++ lib/spree_static_content/engine.rb
@@ -10,6 +10,22 @@ module SpreeStaticContent
       Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
+      if SolidusSupport.solidus_gem_version >= Gem::Version.new("1.4.0")
+        Spree::Backend::Config.configure do |config|
+          config.menu_items << config.class::MenuItem.new(
+            [:pages],
+            'file-text',
+            condition: -> { can?(:manage, Spree::Page) },
+          )
+        end
+      else
+        Deface::Override.new(
+          virtual_path: "spree/admin/shared/_menu",
+          name: "static_content_pages_admin_tab",
+          insert_bottom: "[data-hook='admin_tabs']",
+          partial: "spree/admin/shared/static_content_admin_tab"
+        )
+      end
     end
 
     config.to_prepare &method(:activate).to_proc
