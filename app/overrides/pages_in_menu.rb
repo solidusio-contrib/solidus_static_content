5ad0bfec748cfcf1184e5f71b59de608f7078bc0
Memo Toro <memo.toro@gmail.com>
2017-10-19 22:11:42 +0100

    Fixes visibility hidden of Page menu before login

--- app/overrides/pages_in_menu.rb
+++ app/overrides/pages_in_menu.rb
@@ -1,5 +1,10 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "static_content_pages_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:pages, label: 'Pages', icon: 'file-text') %>",
-                     :disabled => false)
+                     :disabled => false) do
+                      <<-HTML
+                        <% if can? :admin, Spree::Page %>
+                          <%= tab(:pages, label: 'Pages', icon: 'file-text') %>
+                        <% end %>
+                      HTML
+                     end
